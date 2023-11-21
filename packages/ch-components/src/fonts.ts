2138102a80f7a38d1d735ef6880bcc33d57ad2ad
Joao Pilger <joao.pilger@glim.com.br>
2023-11-20 23:34:50 -0300

    first ommit

---
+++ packages/ch-components/src/fonts.ts
@@ -0,0 +1,21 @@
+export const fonts = {
+    fontSizes: {
+        '2xl': '2rem',
+        xl: '1.5rem',
+        lg: '1.25rem',
+        base: '1rem',
+        sm: '0.875rem',
+    },
+    fontWeights: {
+        regular: 400,
+        bold: 700
+    },
+    fontFamilies: {
+        default: 'Roboto',
+        secondary: 'Russo One'
+    },
+    lineHeights: {
+        heading: '1.25',
+        base: '1.6'
+    }
+}
---
+++ web/tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'tailwindcss'
+import { colors, spacing, radii, fonts } from 'ch-components'
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    colors,
+    spacing,
+    borderRadius: radii,
+    fontWeight: fonts.fontWeights,
+    fontFamily: {
+      sans: fonts.fontFamilies.default,
+    },
+    fontSize: fonts.fontSizes,
+    lineHeight: fonts.lineHeights,
+
+    
+  },
+  plugins: [],
+}
+export default config
